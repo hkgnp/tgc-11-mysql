dcaad95c2d3419dc778d5e3b393c05f1f5692651
hkgnp <ben@pngs.cc>
2021-03-15 03:40:44 +0000

    learning mysql

--- queries.sql
+++ queries.sql
@@ -32,3 +32,44 @@ insert into Parents (surname, given_name, email)
 /* see all the rows in a table */
 select * from Parents;
 
+-- Create certificates
+create table Certificates (
+    certificate_id int unsigned auto_increment primary key,
+    title varchar(100) not null
+) engine=innodb;
+
+insert into Certificates (title) 
+    values ('Basic Swimming'),
+           ('Basic Lifesaving'),
+           ('Advanced Lifesaving');
+
+select * from Certificates;
+
+create table Venues (
+    venue_id tinyint unsigned auto_increment primary key,
+    `name` varchar(100)  not null,
+    `address` varchar(500) not null
+) engine=innodb;
+
+insert into Venues (`name`, address) values
+    ('Yishun Swimming Complex', 'Yishun Ave 4'),
+    ('Ang Mio Ko Swimming Complex', 'Ang Mio Kio Ave 1');
+
+describe venues; 
+
+--Foreign keys
+
+create table Students (
+    student_id int unsigned auto_increment primary key,
+    surname varchar(100) not null,
+    given_name varchar(100) not null,
+    date_of_birth date not null,
+    parent_id int unsigned not null,
+    foreign key (parent_id) references Parents(parent_id)
+) engine=innodb;
+
+insert into Students (surname, given_name, date_of_birth, parent_id)
+    values ('Phua', 'Anthony', '1990-06-21', 1);
+
+insert into Students (surname, given_name, date_of_birth, parent_id)
+    values ('Lim', 'Mary', '1990-10-12', 2);
