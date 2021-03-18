d62c9206497b3a15eacb57ef62d64c4c4ebc6e33
hkgnp <ben@pngs.cc>
2021-03-18 04:35:29 +0000

    Completed hands on for chinook

--- handson2.sql
+++ handson2.sql
@@ -1,10 +1,10 @@
 -- 1 - Display all Sales Support Agents with their first name and last name
 
-SELECT FirstName, LastName from Employee;
+SELECT FirstName, LastName from Employee WHERE Title = "Sales Support Agent"
 
 -- 2 - Display all employees hired between 2002 and 2003, and display their first name and last name
 
-SELECT FirstName, LastName
+SELECT FirstName, LastName, HireDate
 FROM Employee
 WHERE YEAR(HireDate) >= 2002 AND YEAR(HireDate) <= 2003;
 
@@ -22,7 +22,7 @@ WHERE Title LIKE "%sales%";
 
 -- 5 - Display the titles of all tracks which has the genre "easy listening"
 
-SELECT Track.Name
+SELECT Track.Name, Genre.Name
 FROM Track
 JOIN Genre 
     ON Track.GenreId = Genre.GenreId
